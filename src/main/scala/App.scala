7e459f0e03c1580b26451e5d9a8aa9d612eeb389
bruce <brucemelodf@gmail.com>
2022-08-07 00:29:28 -0300

    rest api with zio, zhttp, quill and h2.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -9,13 +9,8 @@ object App extends ZIOAppDefault {
   def run =
     Server.start(
       port = 8080,
-//      http = HomeApp() ++ DownloadApp() ++ CounterApp() ++ UserApp()
       http = HomeApp() ++ StudentApp()
     ).provide(
-      // An layer responsible for storing the state of the `counterApp`
-      ZLayer.fromZIO(Ref.make(0)),
-
-      // To use the persistence layer, provide the `PersistentUserRepo.layer` layer instead
       InmemoryStudentRepo.layer
     )
 
